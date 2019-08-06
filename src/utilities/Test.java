21e9e35d7e285b7c0b5afa386061d944533d5623
gihanrcg <it17016230@my.sliit.lk>
2019-08-06 14:21:01 +0530

    Added project description

---
+++ src/utilities/Test.java
@@ -0,0 +1,27 @@
+/**
+ * Code Complexity Analayser for SPM Module (2019)
+ * All Rights Recieved
+ * 
+ * This program is protected by copyright law and by international
+ * conventions. All licensing, renting, lending or copying (including
+ * for private use), and all other use of the program, which is not
+ * expressively permitted by the Development Team, is a
+ * violation of the rights of IFS. Such violations will be reported to the
+ * appropriate authorities.
+ * 
+ * VIOLATIONS OF ANY COPYRIGHT IS PUNISHABLE BY LAW AND CAN LEAD
+ * TO UP TO TWO YEARS OF IMPRISONMENT AND LIABILITY TO PAY DAMAGES.
+ * 
+ * gisilk
+ * Aug 6, 2019
+ */
+ 
+package utilities;
+
+/**
+ * @author gisilk
+ *
+ */
+public class Test {
+
+}
