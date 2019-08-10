d4afc69a9e84c56947c89ea8065e9a2e050311eb
lakshitha <lmvithanage@gmail.com>
2019-08-11 03:22:25 +0530

    tests
    Signed-off-by: lakshitha <lmvithanage@gmail.com>

--- src/test/coreFunctions/ComplexityTest.java
+++ src/test/coreFunctions/ComplexityTest.java
@@ -18,29 +18,30 @@ public class ComplexityTest {
 
         ComplexityDueToSize complexityDueToSize = new ComplexityDueToSize(file);
         assertEquals("[ Line Number 6] : 1\n" +
+                "[ Line Number 8] : 1\n" +
+                "[ Line Number 9] : 1\n" +
                 "[ Line Number 10] : 1\n" +
                 "[ Line Number 11] : 1\n" +
                 "[ Line Number 12] : 1\n" +
-                "[ Line Number 13] : 1\n" +
-                "[ Line Number 14] : 4\n" +
+                "[ Line Number 13] : 4\n" +
+                "[ Line Number 14] : 1\n" +
                 "[ Line Number 15] : 1\n" +
                 "[ Line Number 16] : 1\n" +
                 "[ Line Number 17] : 1\n" +
-                "[ Line Number 18] : 1\n" +
-                "[ Line Number 21] : 1\n" +
-                "[ Line Number 24] : 1\n" +
-                "[ Line Number 27] : 1\n" +
-                "[ Line Number 39] : 2\n" +
-                "[ Line Number 41] : 3\n" +
-                "[ Line Number 44] : 2\n" +
-                "[ Line Number 46] : 2\n" +
-                "[ Line Number 49] : 2\n" +
-                "[ Line Number 51] : 2\n" +
-                "[ Line Number 54] : 2\n" +
-                "[ Line Number 56] : 2\n" +
+                "[ Line Number 20] : 1\n" +
+                "[ Line Number 23] : 1\n" +
+                "[ Line Number 26] : 1\n" +
+                "[ Line Number 38] : 2\n" +
+                "[ Line Number 39] : 3\n" +
+                "[ Line Number 43] : 2\n" +
+                "[ Line Number 44] : 3\n" +
+                "[ Line Number 48] : 2\n" +
+                "[ Line Number 49] : 3\n" +
+                "[ Line Number 53] : 2\n" +
+                "[ Line Number 54] : 3\n" +
                 "\n" +
                 "\n" +
-                "[Total Complexity Score ] : 33\n", complexityDueToSize.getComplexityAnalysisResult());
+                "[Total Complexity Score ] : 37\n", complexityDueToSize.getComplexityAnalysisResult());
 
     }
 }
--- src/test/resources/ComplexityTestFile.java
+++ src/test/resources/ComplexityTestFile.java
@@ -3,29 +3,28 @@ package test.resources;
 import java.util.Scanner;
 
 public class ComplexityTestFile {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
-        int num1,num2;
+        Scanner inp = new Scanner(System.in);
+        int num1, num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
         int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-        switch (choose){
+        switch (choose) {
             case 1:
-                System.out.println(add( num1,num2));
+                System.out.println(add(num1, num2));
                 break;
             case 2:
-                System.out.println(sub( num1,num2));
+                System.out.println(sub(num1, num2));
                 break;
             case 3:
-                System.out.println(mult( num1,num2));
+                System.out.println(mult(num1, num2));
                 break;
             case 4:
-                System.out.println(div( num1,num2));
+                System.out.println(div(num1, num2));
                 break;
             default:
                 System.out.println("Illigal Operation");
@@ -34,27 +33,27 @@ public class ComplexityTestFile {
         }
 
 
-
     }
-    public static int add(int x, int y)
-    {
+
+    public static int add(int x, int y) {
         int result = x + y;
         return result;
     }
-    public static int sub(int x, int y)
-    {
-        int result = x-y;
+
+    public static int sub(int x, int y) {
+        int result = x - y;
         return result;
     }
-    public static int mult(int x, int y)
-    {
-        int result = x*y;
+
+    public static int mult(int x, int y) {
+        int result = x * y;
         return result;
     }
-    public static int div(int x, int y)
-    {
-        int result = x/y;
+
+    public static int div(int x, int y) {
+        int result = x / y;
         return result;
     }
+
 
 }
