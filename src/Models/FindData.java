65107f15f3db5ff44bc650ff8150f385fa689f33
lmvithanage <sanjayajayasinghe54@gmail.com>
2019-08-11 10:11:06 +0530

    Find data clasa added

---
+++ src/Models/FindData.java
@@ -0,0 +1,32 @@
+package Models;
+
+import java.io.File;
+
+public class FindData {
+	private  File file;
+	private  Integer line;
+	
+	
+	
+	public FindData(File file, Integer line) {
+		super();
+		this.file = file;
+		this.line = line;
+	}
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
+	public Integer getLine() {
+		return line;
+	}
+	public void setLine(Integer line) {
+		this.line = line;
+	}
+	
+	
+	
+	
+}
